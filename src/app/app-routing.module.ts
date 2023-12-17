7b2bba76306f5397c7f71a7dfa44fd78ade2082d
ravenstudio <ravenstudio.team@gmail.com>
2023-12-17 09:46:17 +0700

    Deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   {   
     path: 'home', 
